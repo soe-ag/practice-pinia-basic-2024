a11a1cce66b7485b21545fdc0c3d53df54d69881
wind-ik <soeag.st@gmail.com>
2024-03-11 23:24:31 +0900

    added cartStore

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import products from "@/data/products.json";
+// import products from "@/data/products.json";
 import { useProductStore } from "./stores/ProductStore";
+// import { storeToRefs } from "pinia";
 
-useProductStore();
+import { useCartStore } from "./stores/CartStore";
+
+const productStore = useProductStore();
+// use above or destruccture and use it as below, need store to refs to maintain reactivity
+// only good when calling state from stores only, not actions
+// const { products } = storeToRefs(useProductStore());
+
+const cartStore = useCartStore();
+
+productStore.fill();
+
+// comment out the following part as using the actions inside CartStore.js
+// const addToCart = (count, product) => {
+//   count = parseInt(count);
+//   cartStore.$patch((state) => {
+//     for (let index = 0; index < count; index++) {
+//       state.items.push(product);
+//     }
+//   });
+// };
 </script>
 
 <template>
@@ -12,9 +32,10 @@ useProductStore();
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
-        v-for="product in products"
+        v-for="product in productStore.products"
         :key="product.name"
         :product="product"
+        @addToCart="cartStore.addItems($event, product)"
       />
     </ul>
   </div>
--- src/stores/ProductStore.js
+++ src/stores/ProductStore.js
@@ -1,10 +1,20 @@
 import { defineStore } from "pinia";
-import products from "@/data/products.json";
+// import productsJSON from "@/data/products.json"; << this is static import of JSON
 export const useProductStore = defineStore("ProductStore", {
   // state
   state: () => {
-    return { products };
+    // return { products };
+    return {
+      products: [],
+    };
   },
   // actions
+  actions: {
+    async fill() {
+      // this.products = productsJSON; < this is static call
+      this.products = (await import("@/data/products.json")).default;
+      // dynamic import returns that json under a key of default
+    },
+  },
   // getters
 });
